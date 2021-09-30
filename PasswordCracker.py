9aed832d8d0e817a04ae69aa2cb34c9c413f7056
iareebahmad <54855570+iareebahmad@users.noreply.github.com>
2021-09-30 23:52:39 +0530

    Added new characters to password list
    this contribution shall now crack even the passwords with space as a character.

--- PasswordCracker.py
+++ PasswordCracker.py
@@ -1,14 +1,14 @@
 from random import *
-actualpwd = input("Hello User! Enter your password : ")
+actualpwd = input("Hello User! Enter your exisiting password : ")
 password = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
             'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
             'w', 'x', 'y', 'z',0,1,2,3,4,5,6,7,8,9,'!','@','#','$','%','&','*','-','_','=','+','/','A','B','C','D','E','F','G','H','I'
-            ,'J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-myguess = ""
-while (myguess != actualpwd):
-    myguess = ""
+            ,'J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ']
+myguesspass = ""
+while (myguesspass != actualpwd):
+    myguesspass = ""
     for i in range(len(actualpwd)):
         guessletter = password[randint(0, 73)]
-        myguess = str(guessletter) + str(myguess)
-    print(myguess)
-print("Your password is : ",myguess)
+        myguesspass = str(guessletter) + str(myguesspass)
+    print(myguesspass)
+print("We have guessed your password, it is :  ",myguesspass)
