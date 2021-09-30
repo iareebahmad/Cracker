e01817fd20b746b4af931ceb3488cc2875321e1b
iareebahmad <54855570+iareebahmad@users.noreply.github.com>
2021-10-01 00:22:58 +0530

    improved password case list
    password case improved

--- PasswordCracker.py
+++ PasswordCracker.py
@@ -1,9 +1,9 @@
 from random import *
 actualpwd = input("Hello User! Enter your exisiting password : ")
-password = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+password = ['A','B','C','D','E','F','G','H','I'
+            ,'J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',0,1,2,3,4,5,6,7,8,9,'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
             'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-            'w', 'x', 'y', 'z',0,1,2,3,4,5,6,7,8,9,'!','@','#','$','%','&','*','-','_','=','+','/','A','B','C','D','E','F','G','H','I'
-            ,'J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ']
+            'w', 'x', 'y', 'z','!','@','#','$','%','&','*','-','_','=','+','/',' ']
 myguesspass = ""
 while (myguesspass != actualpwd):
     myguesspass = ""
